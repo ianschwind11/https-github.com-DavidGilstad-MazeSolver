360bc972891390a566cc31c2091d86a42c7ccadd
David Gilstad <david.gilstad@ou.edu>
2020-10-09 13:17:49 -0500

    begin maze solver project using reinforcement learning

---
+++ MazeSolver.py
@@ -0,0 +1,10 @@
+#
+# Maze Solver
+#
+# Machine Learning project on reinforcement learning by teaching
+# an agent how to go through a maze in optimal steps without running
+# into objects along the way
+#
+#	authors: David Gilstad, Ian Schwind
+#	created: October 9, 2020
+#
